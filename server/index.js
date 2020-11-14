58b6ff2ca583b88c36040dd21c124ce7844553b4
Sally <sallyazulay@gmail.com>
2020-11-14 22:10:17 +0200

    fixed mongo connection

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes) //added prefix to all routes in posts.js
 
 const CONNECTION_URL = "mongodb+srv://sallyaz:sallyaz123@cluster0.13vl5.azure.mongodb.net/Posts?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // set the connection to the DB
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
