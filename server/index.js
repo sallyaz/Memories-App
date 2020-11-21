3909ce31fe3c094ac1de443b8d46514d29269a3a
Sally <sallyazulay@gmail.com>
2020-11-21 23:12:17 +0200

    added url to the server listen

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5001;
 
 // set the connection to the DB
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, ()=> console.log('server running on port '+ PORT)))
-.catch((error)=> console.log(error.message));
+.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+.catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
