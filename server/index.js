e612f6c245aa3401b818cc4aa75de4da33a2b636
Sally <sallyazulay@gmail.com>
2020-11-22 16:59:23 +0200

    small changes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes) //added prefix to all routes in posts.js
+app.use("/posts", postRoutes); //added prefix to all routes in posts.js
 
-const CONNECTION_URL = "mongodb+srv://sallyaz:sallyaz123@cluster0.13vl5.azure.mongodb.net/Posts?retryWrites=true&w=majority";
+const CONNECTION_URL =
+  "mongodb+srv://sallyaz:sallyaz123@cluster0.13vl5.azure.mongodb.net/Posts?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5001;
 
 // set the connection to the DB
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-.catch((error) => console.log(`${error} did not connect`));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
